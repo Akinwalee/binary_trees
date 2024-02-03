4ded5f9a7a0df60611f8c173b064a739ebb78c36
Akinwalee <obatulafuad@gmail.com>
2024-02-03 15:50:34 +0000

    Updated files

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -13,7 +13,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if (node->left && node->right)
+	if (node->left || node->right)
 		return (0);
 	else
 		return (1);
